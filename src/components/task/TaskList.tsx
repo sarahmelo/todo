0fc5edf6d21001c0aa1e51d35fe16ba2f4833a42
¨sarahmelo¨ <sarahcbdg@gmail.com>
2023-01-07 00:10:04 -0300

    change is complete state the task

--- src/components/task/TaskList.tsx
+++ src/components/task/TaskList.tsx
@@ -1,5 +1,5 @@
-import { Dispatch, useState } from "react";
-import { FiTrash2 } from "react-icons/fi";
+import { Dispatch, ReactElement, ReactHTMLElement, useState } from "react";
+import { FiCheck, FiTrash2 } from "react-icons/fi";
 import { ITask } from "../../interface/task.interface";
 import styles from './TaskList.module.css'
 
@@ -9,11 +9,64 @@ export interface TaskComponent {
 }
 
 export function TaskList({ tasks, setTaskList }: TaskComponent) {
-    function removeTask(id: number): void {
-        const newStateTaskList = tasks.filter(t => t.id !== id)
+    const [inputAttributeChecked, setInputAttributeChecked] = useState<boolean>()
+    const [completeStats, setCompleteStatus] = useState<boolean>();
 
-        
+    function removeTask(id: number): void {
+        const newStateTaskList = tasks.filter(task => task.id !== id)
+
         setTaskList(newStateTaskList)
+    }
+
+    function setInputCheckedState(event: any, task: ITask): void {
+        //console.log(tasks.filter(t => t.name !==  task.name))
+
+        let reAtribuitionTasks = tasks.filter(t => t.name !==  task.name);
+
+        const getTaskPosition = tasks.indexOf(task)
+
+        let getTask: ITask = tasks[getTaskPosition]
+
+
+        if (getTask.complete === true) {
+            getTask.complete = false;
+            setTaskList([...reAtribuitionTasks, getTask])
+            console.log('===> task:',getTask)
+            console.log('tasks:', tasks)
+
+            return;
+        }
+
+        getTask.complete = true;
+        setTaskList([...reAtribuitionTasks, getTask])
+
+
+
+        console.log('===> task:',getTask)
+        console.log('tasks:', tasks)
+
+        // setTaskList()
+        
+        //setInputAttributeChecked(event.target.checked)
+    }
+
+    function setInputClassName(): string {
+        if (!inputAttributeChecked) {
+            return styles.defaultInput
+        }
+
+        return styles.checkedInput
+    }
+
+    let taskIsComplete = (): boolean => inputAttributeChecked === true;
+    let checkedIcon: ReactElement<HTMLImageElement> = <img src="src/assets/checked.svg" alt="" />
+
+    function setTextClassName(): string {
+        if (inputAttributeChecked) {
+            return styles.completeTaskName
+        }
+
+        return styles.taskName
     }
 
     return (
@@ -28,15 +81,29 @@ export function TaskList({ tasks, setTaskList }: TaskComponent) {
                                 className={styles.box}
                             >
                                 <div className={styles.leftContent}>
+                                    <label 
+                                        htmlFor={task.name}
+                                        className={setInputClassName()}    
+                                    >
+                                        
+                                        {
+                                            taskIsComplete()
+                                            ? checkedIcon
+                                            : ''
+                                        }
+                                    </label>
                                     <input 
                                         type="checkbox" 
                                         id={task.name} 
+                                        checked={task.complete}
+                                        onChange={(event) => setInputCheckedState(event, task)}
                                     />
-                                    <p className={styles.taskName}>{task.name}</p>
+                                    <p className={setTextClassName()}>{task.name}</p>
                                 </div>
                                 <button 
                                     className={styles.trash}
-                                    onClick={() => removeTask(task.id)}>
+                                    onClick={() => removeTask(task.id)}
+                                >
                                     <FiTrash2 display={'block'} />
                                 </button>
                             </label>
